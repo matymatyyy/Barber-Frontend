1bab5bc54d1cab6c4f8bb249be3ad80ea85b384b
MatiasCirulli <lococirulli23@gmail.com>
2025-09-25 16:57:56 -0300

    feat: creation of entity services fetchs

--- src/services/serviceService.js
+++ src/services/serviceService.js
@@ -17,3 +17,73 @@ export async function fetchServices(token) {
   const data = await response.json();
   return data.data || data;
 }
+
+export async function fetchSingularService(token, id) {
+  const response = await fetch(`${API_BASE_URL}${API_ENDPOINTS.SERVICES}${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": token,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Token inválido o sesión expirada");
+  }
+
+  const data = await response.json();
+  return data.data || data;
+}
+
+export async function fetchDeleteService(token, id) {
+  const response = await fetch(`${API_BASE_URL}${API_ENDPOINTS.SERVICES}${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": token,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Token inválido o sesión expirada");
+  }
+
+  const data = await response.json();
+  return data.data || data;
+}
+
+export async function fetchUpdateService(token, id) {
+  const response = await fetch(`${API_BASE_URL}${API_ENDPOINTS.SERVICES}${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": token,
+    },
+    body: JSON.stringify({ type, price}),
+  });
+
+  if (!response.ok) {
+    throw new Error("Token inválido o sesión expirada");
+  }
+
+  const data = await response.json();
+  return data.data || data;
+}
+
+export async function fetchCreateService(token) {
+  const response = await fetch(`${API_BASE_URL}${API_ENDPOINTS.SERVICES}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": token,
+    },
+    body: JSON.stringify({ type, price}),
+  });
+
+  if (!response.ok) {
+    throw new Error("Token inválido o sesión expirada");
+  }
+
+  const data = await response.json();
+  return data.data || data;
+}
